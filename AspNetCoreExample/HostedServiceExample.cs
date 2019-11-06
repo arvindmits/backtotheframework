807697e3b2467a161c5768f0d73be85dcf59f763
Oscar van Tol <o.van.tol@betabit.nl>
2019-11-06 23:03:33 +0100

    Added old and new aspnet core

---
+++ AspNetCoreExample/HostedServiceExample.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AspNetCoreExample
+{
+    public class HostedServiceExample : IHostedService
+    {
+        private ILogger _logger;
+
+        public HostedServiceExample(ILogger<HostedServiceExample> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("================================== Starting service ==================================");
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stopping service");
+        }
+    }
+}
