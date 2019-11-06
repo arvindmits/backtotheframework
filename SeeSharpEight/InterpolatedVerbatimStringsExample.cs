5a5170c1fd5f263b27e3b7f0932aef2a65791cc2
Oscar van Tol <o.van.tol@betabit.nl>
2019-11-06 22:01:35 +0100

    Added nullrefs

--- SeeSharpEight/InterpolatedVerbatimStringsExample.cs
+++ SeeSharpEight/InterpolatedVerbatimStringsExample.cs
@@ -8,7 +8,7 @@ namespace SeeSharpEight
 {
     public class InterpolatedVerbatimStringsExample
     {
-        string _c7 = $@"You could do this ¯\_(ツ)_/¯ {DateTime.UtcNow}";
-        string _c8 = @$"Now you can do this ¯\_(ツ)_/¯ {DateTime.UtcNow}";
+        public string CSharp7 = $@"You could do this ¯\_(ツ)_/¯ {DateTime.UtcNow}";
+        public string CSharp8 = @$"Now you can do this ¯\_(ツ)_/¯ {DateTime.UtcNow}";
     }
 }
---
+++ SeeSharpEight/NullableReferenceTypesExample.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeSharpEight
+{
+    public class NullableReferenceTypesExampleDemo
+    {
+        public static void Run()
+        {
+            var nrte = new NullableReferenceTypesExample();
+            nrte.DoSomething();
+            nrte.SetData(null, null, 12);
+        }
+    }
+
+    public class NullableReferenceTypesExample
+    {
+        string _emailaddress;
+        SomeClass _someObject;
+        int _luckyNumber;
+
+        public NullableReferenceTypesExample()
+        {
+
+        }
+
+        public void SetData(string emailAddress, SomeClass someObject, int luckyNumber)
+        {
+            _emailaddress = emailAddress;
+            _someObject = someObject;
+            _luckyNumber = luckyNumber;
+        }
+
+        public void ResetData()
+        {
+            _emailaddress = default;
+            _someObject = default;
+            _luckyNumber = default;
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine(_someObject.Message);
+            Console.WriteLine(_emailaddress.Length);
+            Console.WriteLine(_luckyNumber);
+        }
+    }
+
+    public class SomeClass
+    {
+        public string Message { get; set; } = "Hello world";
+    }
+
+}
